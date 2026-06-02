a70aa819a918717c1a7d5bcce35cb37562bb46e1
Adam Aly <106884713+Adbotdoescode@users.noreply.github.com>
2026-06-02 09:37:45 -0400

    Refactor RobotInfoRecord class and add new constructor

--- RobotInfoRecord.java
+++ RobotInfoRecord.java
@@ -1,72 +1,53 @@
-package g12CP_FinalProject;
+package final_project_2026;
 
 public class RobotInfoRecord {
-	//instance variables to store robot data
-		private int id;
-		private int street; 
-		private int avenue;
-		private int speed;
-		private int safeSpotAve;
-		private int safeSpotStreet;
-		private int itemsCarried;
-		private boolean isZombie;
-		
-		//constructor to set data and intialize record
-		public RobotInfoRecord(int id, int street, int avenue, int speed, boolean isZombie) {
-			this.id = id;
-	        this.street = street;
-	        this.avenue = avenue;
-	        this.speed = speed;
-	        this.isZombie = isZombie;
-		}
-		
-		//getter methods ----->
-		 
-		/**
-		 * method to return the id of the robot
-		 * @return int robot Id
-		 */
-		public int getId() {
-			return this.id;
-		}
-		
-		/**
-		 * method to return the current street the robot is on
-	 	 * @return int robot street
-		 */
-		public int getStreet() {
-			return this.street;
-		}
-		
-		/**
-		 * method to return the current avenue of the robot
-		 * @return int robot avenue
-		 */
-		public int getAvenue() {
-			return this.avenue;
-		}
-		
-		/**
-		 * method to return the speed of the robot
-		 * @return int robot speed
-		 */
-		public int getSpeed() {
-			return this.speed;
-		}
-		
-		/**
-		 * method to return the zombie status of the robot
-		 * @return boolean zombie status
-		 */
-		public boolean getIsZombie() {
-			return this.isZombie;
-		}
-		
-		/**
-		 * method to return amount of items this robot is carrying
-		 * @return int amount of things
-		 */
-		public int getItemsCarried() {
-			return this.itemsCarried;
-		}
+    // instance variables to store robot data
+    private int id;
+    private int street; 
+    private int avenue;
+    private int speed;
+    private int safeSpotAve;
+    private int safeSpotStreet;
+    private int itemsCarried;
+    private boolean isZombie;
+    
+    // constructor to set data and intialize record
+    public RobotInfoRecord(int id, int street, int avenue, int speed, boolean isZombie, int itemsCarried) {
+        this.id = id;
+        this.street = street;
+        this.avenue = avenue;
+        this.speed = speed;
+        this.isZombie = isZombie;
+        this.itemsCarried = itemsCarried;
+    }
+    
+    public RobotInfoRecord(int id2, int street2, int avenue2, double speed2, boolean zombie) {
+        this.street = street;
+        this.avenue = avenue;
+        this.speed = speed;
+        this.isZombie = isZombie;
+    }
+	public int getId() {
+        return this.id;
+    }
+    
+    public int getStreet() {
+        return this.street;
+    }
+    
+    public int getAvenue() {
+        return this.avenue;
+    }
+    
+    public int getSpeed() {
+        return this.speed;
+    }
+    
+    public boolean getIsZombie() {
+        return this.isZombie;
+    }
+    
+    public int getItemsCarried() {
+        return this.itemsCarried;
+    }
 }
