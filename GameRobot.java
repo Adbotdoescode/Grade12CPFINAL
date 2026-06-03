4a6cd443043966926d5317ffb6a53a6361d6ee4c
Adam Aly <106884713+Adbotdoescode@users.noreply.github.com>
2026-06-03 10:08:29 -0400

    Enhance GameRobot with combat ability and role methods
    Added ability and role methods to GameRobot class.

--- GameRobot.java
+++ GameRobot.java
@@ -4,42 +4,49 @@ import becker.robots.*;
 
 public abstract class GameRobot extends RobotSE {
 
-    // protected variables so the medic zombie and survivor can access them
-    protected int id;
-    protected int speed;
-    protected boolean isZombie;
+	// protected variables so the medic zombie and survivor can access them
+	protected int id;
+	protected int speed;
+	protected boolean isZombie;
+	protected int ability;
 
-    public GameRobot(City c, int st, int ave, Direction dir, int id, int speed, boolean isZombie) {
-        super(c, st, ave, dir);
-        this.id = id;
-        this.speed = speed;
-        this.isZombie = isZombie;
-    }
+	public GameRobot(City c, int st, int ave, Direction dir, int id, int speed, boolean isZombie) {
+		super(c, st, ave, dir);
+		this.id = id;
+		this.speed = speed;
+		this.isZombie = isZombie;
+	}
 
-    public abstract TurnAction takeTurn(RobotInfoRecord[] state);
+	public abstract TurnAction takeTurn(RobotInfoRecord[] state);
 
-    public abstract RobotInfoRecord generateRecord();
+	public abstract RobotInfoRecord generateRecord();
 
-    protected double calculateDistance(int targetStreet, int targetAvenue) {
-        int horizontalDistance = targetAvenue - this.getAvenue();
-        int verticalDistance = targetStreet - this.getStreet();
-        double distance = Math.sqrt((horizontalDistance * horizontalDistance) + (verticalDistance * verticalDistance));
-        return distance; 
-    }
+	protected double calculateDistance(int targetStreet, int targetAvenue) {
+		int horizontalDistance = targetAvenue - this.getAvenue();
+		int verticalDistance = targetStreet - this.getStreet();
+		double distance = Math.sqrt((horizontalDistance * horizontalDistance) + (verticalDistance * verticalDistance));
+		return distance; 
+	}
 
-    public int getId() {
-        return this.id; 
-    }
-    public double getSpeed() { 
-        return this.speed; 
-    }
-    public boolean isZombie() { 
-        return this.isZombie; 
-    }
-    
-    /**
-     * gets the combat stat of the robot for dice rolls
-     * @return int combat ability value
+	public int getId() {
+		return this.id; 
+	}
+	public double getSpeed() { 
+		return this.speed; 
+	}
+	public boolean isZombie() { 
+		return this.isZombie; 
+	}
+
+	/**
+	 * gets the combat stat of the robot for dice rolls
+	 * @return int combat ability value
+	 */
+	public abstract int getCombatAbility();
+	
+	/**
+     * gets the role of the robot for turn order and safe zone logic
+     * @return string the role of the robot
      */
-    public abstract int getCombatAbility();
+    public abstract String getRole();
 }
