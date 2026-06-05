c7d5f6a1fc89b64b8f0b4f7d491407e88f33e886
Adam Aly <106884713+Adbotdoescode@users.noreply.github.com>
2026-06-05 09:59:22 -0400

    Update MedicRobot.java

--- MedicRobot.java
+++ MedicRobot.java
@@ -78,7 +78,7 @@ public class MedicRobot extends GameRobot {
 			for (int i = 0; i < state.length; i++) { 
 
 				// If the robot is a zombie, then add its record to the zombieRecords array
-				if (state[i].getIsZombie() == true) { 
+				if (state[i].getIsZombie() == true && state[i].getId() != this.getId()) { 
 					zombieRecords.add(new ZombieInfoRecord(state[i].getId(), state[i].getStreet(), state[i].getAvenue(), state[i].getSpeed(), state[i].getIsZombie(), calculateDistance(this.getStreet(), this.getAvenue() ,state[i].getStreet(), state[i].getAvenue())));
 				}
 			}
@@ -196,10 +196,6 @@ public class MedicRobot extends GameRobot {
 
 	}
 
-	@Override
-	public RobotInfoRecord generateRecord() {
-		return new RobotInfoRecord(this.getId(), this.getStreet(), this.getAvenue(), this.speed, this.isZombie(), 0);
-	}
 
 	/**
 	 * This method calculates the direct distance from one point to another using pythagorean theorem
